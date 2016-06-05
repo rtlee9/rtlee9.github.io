1a5c7c2367a982bcb522d4f34f5345c6bc44e384
Ryan Lee <ryantlee9@gmail.com>
2016-06-04 22:59:45 -0400

    Test Rmd post

---
+++ KnitPost.R
@@ -0,0 +1,51 @@
+KnitPost <- function(site.path='/pathToYourBlog/', overwriteAll=F, overwriteOne=NULL) {
+  if(!'package:knitr' %in% search()) library('knitr')
+  
+  ## Blog-specific directories.  This will depend on how you organize your blog.
+  site.path <- site.path # directory of jekyll blog (including trailing slash)
+  rmd.path <- paste0(site.path, "_source") # directory where your Rmd-files reside (relative to base)
+  fig.dir <- "assets/Rfig/" # directory to save figures
+  posts.path <- paste0(site.path, "_posts") # directory for converted markdown files
+  cache.path <- paste0(site.path, "_cache") # necessary for plots
+  
+  render_jekyll(highlight = "pygments")
+  opts_knit$set(base.url = '/', base.dir = site.path)
+  opts_chunk$set(fig.path=fig.dir, fig.width=8.5, fig.height=5.25, dev='svg', cache=F, 
+                 warning=F, message=F, cache.path=cache.path, tidy=F)   
+  
+  setwd(rmd.path) # setwd to base
+  
+  # some logic to help us avoid overwriting already existing md files
+  files.rmd <- data.frame(rmd = list.files(path = rmd.path,
+                                           full.names = T,
+                                           pattern = "\\.Rmd$",
+                                           ignore.case = T,
+                                           recursive = F), stringsAsFactors=F)
+  files.rmd$corresponding.md.file <- paste0(posts.path, "/", basename(gsub(pattern = "\\.Rmd$", replacement = ".md", x = files.rmd$rmd)))
+  files.rmd$corresponding.md.exists <- file.exists(files.rmd$corresponding.md.file)
+  
+  ## determining which posts to overwrite from parameters overwriteOne & overwriteAll
+  files.rmd$md.overwriteAll <- overwriteAll
+  if(is.null(overwriteOne)==F) files.rmd$md.overwriteAll[grep(overwriteOne, files.rmd[,'rmd'], ignore.case=T)] <- T
+  files.rmd$md.render <- F
+  for (i in 1:dim(files.rmd)[1]) {
+    if (files.rmd$corresponding.md.exists[i] == F) {
+      files.rmd$md.render[i] <- T
+    }
+    if ((files.rmd$corresponding.md.exists[i] == T) && (files.rmd$md.overwriteAll[i] == T)) {
+      files.rmd$md.render[i] <- T
+    }
+  }
+  
+  # For each Rmd file, render markdown (contingent on the flags set above)
+  for (i in 1:dim(files.rmd)[1]) {
+    if (files.rmd$md.render[i] == T) {
+      out.file <- knit(as.character(files.rmd$rmd[i]), 
+                       output = as.character(files.rmd$corresponding.md.file[i]),
+                       envir = parent.frame(), 
+                       quiet = T)
+      message(paste0("KnitPost(): ", basename(files.rmd$rmd[i])))
+    }     
+  }
+  
+}
