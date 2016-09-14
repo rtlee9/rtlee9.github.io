8bbdd9d81d7ce9a2191d91c88a8e090dc3ea748c
Ryan Lee <ryantlee9@gmail.com>
2016-09-13 20:00:09 -0400

    Switch to Granim for dynamic gradients animation

---
+++ js/granim.js
@@ -0,0 +1,13 @@
+var granimInstance = new Granim({
+   element: '#granim-canvas',
+   name: 'granim',
+   opacity: [1, 1],
+   states : {
+       "default-state": {
+           gradients: [
+               ['#834D9B', '#D04ED6'],
+               ['#1CD8D2', '#93EDC7']
+           ]
+       }
+   }
+});
